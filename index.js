fadb78b19465f980bd6410d3be5774ad3990e21c
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-04-20 16:26:59 -0400

    looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+
+function writeCards(names, event) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {  
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+     
+    }
+return messages
+}
+
+function countDown(number){
+    for(let count = 10; count >=0; count--)
+    console.log(count);
+
+}
